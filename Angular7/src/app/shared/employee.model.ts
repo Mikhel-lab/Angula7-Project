bbef64d539c26ba0e8b1d9223a3250fb13dae873
Michael Aiyetan <michael.aiyetanjnr03@gmail.com>
2020-12-03 16:57:08 -0800

    Angular7-Project

---
+++ Angular7/src/app/shared/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    EmployeeID: number;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    Address: string;
+    Mobile: string;
+    EmpCode: string;
+    Position: string;
+    Username: string;
+    Password: string;
+    Gender: string;
+}
---
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+			//Enable CORS
+			var corsAttr = new EnableCorsAttribute("http://localhost:4800", "*", "*");
+			config.EnableCors(corsAttr);
+			//config.EnableCors(new EnableCorsAttribute("http://localhost:4800", headers: "*", methods: "*"));
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
